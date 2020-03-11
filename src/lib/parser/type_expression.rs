f22c76d6c91bd2bc147b0f761796bcbf418e9e92
noxabellus <noxabellus@gmail.com>
2020-03-10 19:23:43 -0500

    Implemented type expr parser interface + idents

--- src/lib/parser/type_expression.rs
+++ src/lib/parser/type_expression.rs
@@ -1,11 +1,59 @@
 //! The TypeExpression Parser function and its dependencies
 
-use crate::ast::{ TypeExpression };
+use crate::{
+  token::{ Token, TokenKind, TokenData },
+  ast::{ TypeExpression, TypeExpressionData },
+};
 
 use super::Parser;
 
 
+
+struct TypeParselet {
+  predicate: fn (&Token) -> bool,
+  parse: fn (&mut Parser) -> Option<TypeExpression>,
+}
+
+impl TypeParselet {
+  #[inline]
+  fn predicate (&self, token: &Token) -> bool {
+    (self.predicate)(token)
+  }
+
+  #[inline]
+  fn parse (&self, parser: &mut Parser) -> Option<TypeExpression> {
+    (self.parse)(parser)
+  }
+}
+
+
 /// Parse a single TypeExpression
 pub fn type_expression (parser: &mut Parser) -> Option<TypeExpression> {
-  unimplemented!("{:?}", parser.curr_tok())
+  let token = parser.curr_tok()?;
+
+  for parselet in TYPE_PARSELETS.iter() {
+    if parselet.predicate(token) {
+      return parselet.parse(parser)
+    }
+  }
+
+  parser.error("No semantic match for this token in the context of a type expression".to_owned());
+
+  None
 }
+
+
+fn identifier (parser: &mut Parser) -> Option<TypeExpression> {
+  if let Some(Token { data: TokenData::Identifier(ident), origin }) = parser.curr_tok() {
+    let result = Some(TypeExpression::new(TypeExpressionData::Identifier(*ident), *origin));
+    parser.advance();
+    result
+  } else {
+    None
+  }
+}
+
+
+const TYPE_PARSELETS: &[TypeParselet] = &[
+  TypeParselet { predicate: |token| token.kind() == TokenKind::Identifier, parse: identifier },
+];
--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,72 @@ fn main () -> std::io::Result<()> {
   if !ansi::enable() { println!("Failed to enable ansi coloring for terminal") }
   else { println!("\n{}Ansi coloring enabled for terminal{}\n", ansi::Foreground::Green, ansi::Foreground::Reset) }
   
-  let source = Source::load("./test_scripts/min.ms".to_owned())?;
+  { // test lexer
+    let source = Source::load("./test_scripts/min.ms".to_owned())?;
 
-  let mut lexer = Lexer::new(&source);
+    let mut lexer = Lexer::new(&source);
 
-  let stream = lexer.lex_stream();
+    let stream = lexer.lex_stream();
 
-  println!("Lexing complete:\n{}", stream);
+    println!("Lexing complete:\n{}", stream);
 
-  source.notice(None, format!("Test {}", 123));
-  source.warning(None, format!("Test {}", 456));
+    source.notice(None, format!("Test {}", 123));
+    source.warning(None, format!("Test {}", 456));
 
-  let region = SourceRegion {
-    start: SourceLocation {
-      index: source.line_and_column_to_index(17, 11).unwrap(),
-      line: 17,
-      column: 11,
-    },
-    end: SourceLocation {
-      index: source.line_and_column_to_index(19, 11).unwrap(),
-      line: 19,
-      column: 11,
-    },
-  };
+    let region = SourceRegion {
+      start: SourceLocation {
+        index: source.line_and_column_to_index(17, 11).unwrap(),
+        line: 17,
+        column: 11,
+      },
+      end: SourceLocation {
+        index: source.line_and_column_to_index(19, 11).unwrap(),
+        line: 19,
+        column: 11,
+      },
+    };
 
-  source.warning(Some(region), "Theres a problem or whatever".to_string());
+    source.warning(Some(region), "Theres a problem or whatever".to_string());
 
-  source.print_notices();
-  source.print_warnings();
-  source.print_errors();
+    source.print_notices();
+    source.print_warnings();
+    source.print_errors();
+  }
+
+  println!("\n-------------------------\n");
+
+
+  { // test type_expression
+    use mod_language::{ ast::*, parser::{ Parser, type_expression::type_expression } };
+
+
+    let source = Source::load("./test_scripts/type_expression.ms".to_owned())?;
+
+    let mut lexer = Lexer::new(&source);
+
+    let stream = lexer.lex_stream();
+
+    let mut parser = Parser::new(&stream);
+
+    let ident = type_expression(&mut parser);
+
+    if let Some(TypeExpression { data: TypeExpressionData::Identifier(ident), .. }) = ident {
+      println!("Got type identifier {}", ident);
+    } else {
+      panic!("Expected type identifier, got {:#?}", ident);
+    }
+
+    let none = type_expression(&mut parser);
+
+    if none.is_none() && source.messages.borrow().len() == 1 {
+      println!("Got expected error instead of type expression");
+      source.print_errors();
+    } else {
+      source.print_messages();
+      panic!("Expected error, got {:#?}", none);
+    }
+  }
+
 
   Ok(())
 }
