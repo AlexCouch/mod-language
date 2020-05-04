befc1109638253574a55943ba03696fd1d67f8c0
noxabellus <noxabellus@gmail.com>
2020-05-04 18:37:56 -0500

    Updated test app to dump to log files

--- src/main.rs
+++ src/main.rs
@@ -16,28 +16,36 @@ fn main () -> std::io::Result<()> {
   if !ansi::enable() { println!("Failed to enable ansi coloring for terminal") }
   else { println!("\n{}Ansi coloring enabled for terminal{}\n", ansi::Foreground::Green, ansi::Foreground::Reset) }
   
+
   SESSION.init();
   SOURCE_MANAGER.init();
 
+
   let source = SOURCE_MANAGER.load("./test_scripts/item_analysis.ms")?;
+
 
   let mut lexer = Lexer::new(source);
 
   let stream = lexer.lex_stream();
 
-  println!("Lexing complete:\n{:#?}", &stream);
+  println!("Got token stream, dumping to ./stream.log");
+  std::fs::write("./stream.log", format!("{:#?}", stream)).expect("Failed to dump token stream to ./stream.log");
+
 
   let mut parser = Parser::new(&stream);
 
   let ast = parser.parse_ast();
 
-  println!("Got ast: {:#?}", &ast);
+  println!("Got ast, dumping to ./ast.log");
+  std::fs::write("./ast.log", format!("{:#?}", ast)).expect("Failed to dump token ast to ./ast.log");
+
 
   let analyzer = Analyzer::new(&ast);
 
-  let _context = analyzer.analyze();
+  let context = analyzer.analyze();
 
-  // println!("Got context: {:#?}", &context); yikes
+  println!("Got context, dumping to ./context.log");
+  std::fs::write("./context.log", format!("{:#?}", context)).expect("Failed to dump context to ./context.log");
 
   if !SESSION.messages().is_empty() {
     SESSION.print_messages();
