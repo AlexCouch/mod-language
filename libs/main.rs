6b5f8b6fc53b991c18596e0ed873d8b4a55d2ecc
noxabellus <noxabellus@gmail.com>
2020-05-21 18:47:26 -0500

    Made sub crate layout more idiomatic :[

---
+++ libs/main.rs
@@ -0,0 +1,9 @@
+//! A wrapper library for all of mod_language's components
+
+pub extern crate mod_ansi;
+pub extern crate mod_utils;
+pub extern crate mod_common;
+pub extern crate mod_frontend;
+pub extern crate mod_declaration_builder;
+pub extern crate mod_bytecode;
+pub extern crate mod_codegen;
